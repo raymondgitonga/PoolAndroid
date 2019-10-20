1f19d3f6cacc2612b91465312938bc397e4e3ec4
raymond gitonga <raytosh95@gmail.com>
2019-10-20 10:50:48 +0300

    migrate AppIntro to Kotlin

---
+++ app/src/main/java/com/tosh/poolandroid/view/appIntro/IntroAdapter.kt
@@ -0,0 +1,22 @@
+package com.tosh.poolandroid.view.appIntro
+
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class IntroAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    override fun getItem(position: Int): Fragment {
+        when (position) {
+            0 -> return FirstFragment()
+            1 -> return SecondFragment()
+            2 -> return ThirdFragment()
+            else -> return FirstFragment()
+        }
+    }
+
+    override fun getCount(): Int {
+        return 3
+    }
+}
