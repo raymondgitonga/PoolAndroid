c0567a9c781af311882978ce5bde1c5fdfca33d4
raymond gitonga <raytosh95@gmail.com>
2020-01-04 08:08:55 +0300

    Refactor AppIntro to Kotlin

---
+++ app/src/main/java/com/tosh/poolandroid/view/appIntro/ThirdFragment.kt
@@ -0,0 +1,29 @@
+package com.tosh.poolandroid.view.appIntro
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.tosh.poolandroid.R
+import com.tosh.poolandroid.view.activity.LoginActivity
+
+class ThirdFragment : Fragment() {
+    private val getStartedBtn: Button? = null
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? { // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_third, container, false)
+        val btnStarted =
+            view.findViewById<Button>(R.id.btn_getStarted)
+        btnStarted.setOnClickListener {
+            val intent = Intent(activity, LoginActivity::class.java)
+            startActivity(intent)
+            activity!!.finish()
+        }
+        return view
+    }
+}
