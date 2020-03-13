7f0159d63be5c3533973b09dc23ccbe77de5305e
raymond gitonga <raytosh95@gmail.com>
2020-03-14 01:58:40 +0300

    Add orders fragment appbar title

--- app/src/main/java/com/tosh/poolandroid/view/fragment/OrdersFragment.kt
+++ app/src/main/java/com/tosh/poolandroid/view/fragment/OrdersFragment.kt
@@ -5,13 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProviders
 
 import com.tosh.poolandroid.R
+import com.tosh.poolandroid.view.activity.MainActivity
+import com.tosh.poolandroid.viewmodel.MainViewModel
 
 class OrdersFragment : Fragment() {
+
+    private var mainViewModel: MainViewModel? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_orders, container, false)
     }
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mainViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
+        (activity as MainActivity).setupToolbar(getString(R.string.orders))
+    }
 }
